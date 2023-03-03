4984de064c03c54ca708051b74c9171ccb5f6816
zypreaxa <114878563+zypreaxa@users.noreply.github.com>
2023-03-03 13:54:14 +0200

    Delete headers directory

--- headers/Studentas.h
+++
@@ -1,9 +0,0 @@
-﻿struct Studentas {
-	string vardas, pavarde;
-	vector<int> paz; //naudojamas vietoje masyvo rodykles
-	int egz;
-	float galutinis; //apskaičiuojamas pagal pasirinkimą(vidurkį arba medianą).
-};
-
-Studentas temp;
-vector<Studentas> sarasas;
